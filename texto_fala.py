9a44b49c8650bd2e529599c30c8827473c5d241d
Brenno Pacheco <brenno_pacheco@hotmail.com>
2024-01-26 22:37:36 -0300

    audio_automatico

--- texto_fala.py
+++ texto_fala.py
@@ -1,4 +1,5 @@
 from gtts import gTTS
+import os 
 
 texto = "Criando um script para ser transformado de texto para áudio"
 idioma = 'pt'
@@ -8,3 +9,8 @@ tts = gTTS(texto, lang=idioma, tld=sotaque)
 
 # criando a faixa de áudio
 tts.save("audio.mp3")
+
+
+#rodando o áudio de forma automática
+os.system('ffplay audio.mp3')
+
